60fd922204e81d39a8ceaedda5376eb245f68943
Markus Hauru <markus.hauru@gmail.com>
2013-08-12 17:32:43 +0300

    Created type constructor synonym Lattice, reorganised the way neighbourhoods are built and added diagonal couplings.

--- lattice.hs
+++ lattice.hs
@@ -39,16 +39,16 @@ main = do
     mainGUI
     where red = Color 65535 0 0
 
-updateAndDraw :: S.Seq (S.Seq Spin) -> ([Int],[Int]) -> [Double] -> DrawWindow -> CommVars -> GC -> GC -> IO ()
+updateAndDraw :: Lattice Spin -> ([Int],[Int]) -> [Double] -> DrawWindow -> CommVars -> GC -> GC -> IO ()
 updateAndDraw l (risW,risH) rds dw cv gcUp gcDown = do
     let ([x],risW') = splitAt 1 risW
     let ([y],risH') = splitAt 1 risH
     let ([r],rds') = splitAt 1 rds
     t <- withMVar (temp cv) $ return
     coups <- withMVar (couplings cv) $ return
-    let t' = if t==0 then 10^^(-3) else t --"absolute zero"
+    let t' = if t==0 then 10^^(-5) else t --"absolute zero"
     let beta = 1/t'
-    let neigh = neighbourhood l x y
+    let neigh = neighbourhood l (x,y)
     let cand = if S.index (S.index l x) y == Up then Down else Up --Candidate for the new state
     let s' = updateSpin neigh cand beta coups r
     let l' = S.update x (S.update y s' (S.index l x)) l --Can this be done without two calls to x l?
@@ -65,51 +65,97 @@ updateAndDraw l (risW,risH) rds dw cv gcUp gcDown = do
                             --which is an unnecessarily strict requirement for helping drawing keep up with physics.
     updateAndDraw l' (risW',risH') rds' dw cv gcUp gcDown
 
---------
 
-updateSpin :: (Spin,[Spin]) -> Spin -> Double -> [Double] -> Double -> Spin
-updateSpin (s1,neigh) s2 beta coups r = if s2E < s1E || r < boltzmannFactor then s2 else s1
-    where s1E = neighbourhoodE (s1,neigh) coups
-          s2E = neighbourhoodE (s2,neigh) coups
+-------- PHYSICS --------
+
+updateSpin :: (Spin,[Spin],[Spin]) -> Spin -> Double -> [Double] -> Double -> Spin
+updateSpin (s1,nearest,diagonal) s2 beta coups r = if s2E < s1E || r < boltzmannFactor then s2 else s1
+    where s1E = neighbourhoodE (s1,nearest,diagonal) coups
+          s2E = neighbourhoodE (s2,nearest,diagonal) coups
           boltzmannFactor = exp $ (-1) * beta * (s2E - s1E)
 
-neighbourhood :: S.Seq (S.Seq Spin) -> Int -> Int -> (Spin,[Spin])
-neighbourhood l x y = (S.index (S.index l x) y, S.index (S.index l (right x)) y : S.index (S.index l x) (down y) : S.index (S.index l (left x)) y : S.index (S.index l x) (up y) : [])
-    where    --Periodic boundaries
-    right x = if x == S.length l - 1 then 0 else x+1
-    left x = if x == 0 then S.length l - 1 else x-1
-    up y = if y == 0 then S.length (S.index l x) - 1 else y-1
-    down y = if y == S.length (S.index l x) - 1 then 0 else y+1
+neighbourhood :: Lattice Spin -> (Int,Int) -> (Spin,[Spin],[Spin])
+neighbourhood l (x,y) = (at l here (x,y), nearest, diagonal)
+    where nearest = map (\dir -> at l dir (x,y)) [east, south, west, north]
+          diagonal = map (\dir -> at l dir (x,y)) [nE, sE, sW, nW]
 
 --The couplings are passed as a list, feel free to assign each of them to whatever purpose
---you like, but be careful not to request couplings out of range.
---Add sliders as needed, a new coupling will be added automatically.
-
---Ising model
-pairE :: Spin -> Spin -> [Double] -> Double
-pairE s1 s2 coups = (if s1/=s2 then -0.01 else 0.01)*coups!!0 --Bogus units
+--you like, but be careful not to request couplings out of range. TODO could I check against that?
+--Add sliders as needed, a new coupling will be added to the list automatically.
 
 --External field
 singleE :: Spin -> [Double] -> Double
-singleE s coups = (if s==Down then -0.01 else 0.01)*coups!!1
+singleE s coups = (if s==Down then -0.01 else 0.01)*coups!!0 --Bogus units and constant factor
 
-neighbourhoodE :: (Spin,[Spin]) -> [Double] -> Double
-neighbourhoodE (x,xs) coups = (sum $ map (\s -> pairE x s coups) xs) + singleE x coups
+--Ising model
+nearPairE :: Spin -> Spin -> [Double] -> Double
+nearPairE s1 s2 coups = (if s1/=s2 then -0.01 else 0.01)*coups!!1 --Bogus units and constant factor
+
+--Energy of diagonally connected spins
+diagPairE :: Spin -> Spin -> [Double] -> Double
+diagPairE s1 s2 coups = (if s1/=s2 then -0.01 else 0.01)*coups!!2 --Bogus units and constant factor
+
+neighbourhoodE :: (Spin,[Spin],[Spin]) -> [Double] -> Double
+neighbourhoodE (x,nearest,diagonal) coups = singleE x coups + nearestE + diagonalE
+    where nearestE = sum $ map (\s -> nearPairE x s coups) nearest
+          diagonalE = sum $ map (\s -> diagPairE x s coups) diagonal
 
 --------
 
 data Spin = Up | Down deriving (Eq, Show, Read)
 
-downLattice :: Int -> Int -> S.Seq (S.Seq Spin)
+downLattice :: Int -> Int -> Lattice Spin
 downLattice w h = S.replicate w $ S.replicate h Down
 
-upLattice :: Int -> Int -> S.Seq (S.Seq Spin)
+upLattice :: Int -> Int -> Lattice Spin
 upLattice w h = S.replicate w $ S.replicate h Up
 
-randomLattice :: [Double] -> Int -> Int -> S.Seq (S.Seq Spin)
+randomLattice :: [Double] -> Int -> Int -> Lattice Spin
 randomLattice rands w h = S.fromList $ [S.fromList $ map (\d -> if d<0.5 then Up else Down) $ take h $ drop (h*i) rands | i <- [0..w]]
 
 --------
+
+type Lattice a = S.Seq (S.Seq a)
+
+at :: Lattice a -> (Lattice a -> (Int,Int) -> (Int,Int)) -> (Int,Int) -> a
+at l dir (x,y) = S.index (S.index l x') y'
+   where (x',y') = dir l (x,y) 
+
+--A pretty useless function just to make the collection of directions consistent and complete
+here :: Lattice a -> (Int,Int) -> (Int,Int)
+here l = id
+
+--Periodic boundary conditions
+east :: Lattice a -> (Int,Int) -> (Int,Int)
+east l (x,y) = (x',y)
+    where x' = if x == S.length l - 1 then 0 else x+1
+   
+west :: Lattice a -> (Int,Int) -> (Int,Int)
+west l (x,y) = (x',y)
+    where x' = if x == 0 then S.length l - 1 else x-1
+
+north :: Lattice a -> (Int,Int) -> (Int,Int)
+north l (x,y) = (x,y')
+    where y' = if y == 0 then S.length (S.index l x) - 1 else y-1
+
+south :: Lattice a -> (Int,Int) -> (Int,Int)
+south l (x,y) = (x,y')
+    where y' = if y == S.length (S.index l x) - 1 then 0 else y+1
+
+nE :: Lattice a -> (Int,Int) -> (Int,Int)
+nE l = (north l).(east l)
+
+nW :: Lattice a -> (Int,Int) -> (Int,Int)
+nW l = (north l).(west l)
+
+sE :: Lattice a -> (Int,Int) -> (Int,Int)
+sE l = (south l).(east l)
+
+sW :: Lattice a -> (Int,Int) -> (Int,Int)
+sW l = (south l).(west l)
+
+
+-------- GUI STUFF -------- 
 
 --MVars for communication between physics and GUI
 data CommVars = CommVars {
@@ -125,7 +171,7 @@ data GUI = GUI {
     couplingScls :: [HScale],
     whaatBt :: Button}
 
-drawLattice :: DrawWindow -> CommVars -> GC -> GC -> S.Seq (S.Seq Spin) -> IO ()
+drawLattice :: DrawWindow -> CommVars -> GC -> GC -> Lattice Spin -> IO ()
 drawLattice dw cv gcUp gcDown l = do
     (width, height) <- withMVar (canvasSize cv) $ return
     let size = minimum [div width (S.length l), div height (S.length (S.index l 0))] --Assuming rectangular lattice
@@ -141,7 +187,7 @@ loadGlade gladepath = do
     mw <- xmlGetWidget xml castToWindow "mainWindow"
     [whaatBt] <- mapM (xmlGetWidget xml castToButton) ["whaatBt"]
     tempScl <- xmlGetWidget xml castToVScale "tempScl"
-    --Add new couplings here as needed
+    --Add new a new couplingScl here if needed and a new coupling will be available for the energy functions
     couplingScl1 <- xmlGetWidget xml castToHScale "couplingScl1"
     couplingScl2 <- xmlGetWidget xml castToHScale "couplingScl2"
     couplingScl3 <- xmlGetWidget xml castToHScale "couplingScl3"
